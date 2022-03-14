de70f8c880e06f5546e9fc2c8c20b88ed95685f3
Leah Bineth <83653442+LeahBineth@users.noreply.github.com>
2022-03-14 22:14:46 +0200

    remove gluten free from spec

--- spec.sql
+++ spec.sql
@@ -1,10 +1,9 @@
 /*
-AS: GLUTEN FREE OPTION WAS TAKEN OUT OF THE SPEC.
 I recently started a home based baking business, together with my cousin. She works from her home in Lakewood and I work in Brooklyn.
 I sell cakes, cookies, and cupcakes for various events.
 I want to keep a database of all orders.
 I need to know whether they order cookies, cake/s, or cupcakes.
-I need to know the base of cookie/cake, what topping to decorate it with, whether or not it is gluten free, and space to put in their custom order specifications.
+I need to know the base of cookie/cake, what topping to decorate it with, and space to put in their custom order specifications.
 I also want to know if they want a photo on the cake/cookie.
 Please calculates the price of order when entered.
 I need the name of the Customer, branch they ordered from, date of order, amount they bought, how much they cost is per item and how much the whole order was.
@@ -16,13 +15,11 @@ Coconut
 Chocolate Peanut Butter 
 Banana
 Strawberry shortcake
-Chocolate and Vanilla and Banana come in a gluten free option.
-All cookies have same 6 options of flavors and can be gluten free.
 Toppings:
 Royal icing or fondant or frosting - chocolate, caramel, strawberry, coconut, peanut butter
-All cakes are $50, gluten free is extra $20, strawberry shortcake is extra $5
-All cupcakes are $3, gluten free is extra $1
-All cookies are $3.50, gluten free is extra $1
+All cakes are $50, strawberry shortcake is extra $5
+All cupcakes are $3, 
+All cookies are $3.50, 
 Picture adds on $8 for a cake and $1.50 for a cookie. We don't offer pictures on cupcakes.
 I'll need these reports:
 1) Sum of how many of each type of cake/cupcake/cookie is sold per branch 
@@ -32,9 +29,8 @@ I'll need these reports:
     Yom Tov time: September and October
     Winter: November - April
     Spring: May and June
-3) How many gluten free orders per month
-4) How much money came in each month per branch
-5) How many orders came in each month per branch
+3) How much money came in each month per branch
+4) How many orders came in each month per branch
 Question: When is the earliest order?
 Answer: I opened shop January 1, 2021.
 Question: Do you have a minimum or maxmimum
@@ -43,30 +39,30 @@ Answer: Cookies have minimum order of 24 and maximum order of 500.
 Question: Do you need to know the time it is entered?
 Answer: No need to.
 Sample data:
-Name, Branch, Date, Type of base, Item, Topping, Picture?, GF, Specifics, Occasion, Amount, (Price per, Order price)
-Chaim Green, Lakewood, 01/04/2022, Strawberry shortcake, Cake, (no topping), (no picture), (not gf), Please make sure they both look the same for a kiddush, Baby, 2, ($55, $110)
-Rivky Shapiro, Lakewood, 07/22/2021, Chocolate, Cake, Caramel, no, gf, Write Happy birthday on cake, Birthday, 1
-Leah Gross, Brooklyn, 06/11/2021, Sugar, Cookie, Royal icing, yes, gf, Graduation cap shape of picture I emailed, Graduation, 70
-Baruch Goldberg, Brooklyn, 09/12/2021, Sugar, Cookie, Royal icing, no, no, Shape of a mask and write thank you for keeping everyone safe and company logo, Company logo, 500
-Binyamin Stein, Lakewood, 02/15/2021, Vanilla, Cake, Chocolate, yes, gf, Write Happy 85th birthday on bottom of the attached photo, Birthday, 3
+Name, Branch, Date, Type of base, Item, Topping, Picture?, Specifics, Occasion, Amount, (Price per, Order price)
+Chaim Green, Lakewood, 01/04/2022, Strawberry shortcake, Cake, (no topping), (no picture),  Please make sure they both look the same for a kiddush, Baby, 2, ($55, $110)
+Rivky Shapiro, Lakewood, 07/22/2021, Chocolate, Cake, Caramel, no, Write Happy birthday on cake, Birthday, 1
+Leah Gross, Brooklyn, 06/11/2021, Sugar, Cookie, Royal icing, yes,  Graduation cap shape of picture I emailed, Graduation, 70
+Baruch Goldberg, Brooklyn, 09/12/2021, Sugar, Cookie, Royal icing, no, Shape of a mask and write thank you for keeping everyone safe and company logo, Company logo, 500
+Binyamin Stein, Lakewood, 02/15/2021, Vanilla, Cake, Chocolate, yes,  Write Happy 85th birthday on bottom of the attached photo, Birthday, 3
 Batsheva Golden, Lakewood, 08/14/2021, Chocolate peanut butter, Cake, Peanut butter, yes, no, Write Shloimy and the number 3 on pic, Upsherin, 1
-Rena Stern, Brooklyn, 10/11/2021, Sugar, Cookie, Fondant, no, no, Pink background with glitter and shape of balloon with word Sori, Bas mitzvah, 75
-Layala Katz, Lakewood, 09/05/2021, Vanilla, Cupcake, Strawberry, no, no, Make it look nice!, Birthday, 28
-Sara Leah Levy, Brooklyn, 05/18/2021, Vanilla, Cupcake, Coconut, no, no, none, Engagement, 100
-Devorah Friedman, Brooklyn, 07/04/2021, Strawberry shortcake, Cake, no topping, no, no, none, Wedding, 15
-Kaufman, Lakewood, 11/09/2021, Chocolate peanut butter, Cake, Chocolate, no, no, none, Bar mitzvah, 3
-Chana Cohen, Lakewood, 07/04/2021, Banana, Cake, Vanilla, yes, gf, Attached photo, Anniversary, 1
-Ahuva Licht, Lakewood, 06/22/2021, Sugar, Cookie, Royal icing, no, gf, Write Chaim and Devorah, Engagement, 75
+Rena Stern, Brooklyn, 10/11/2021, Sugar, Cookie, Fondant, no, Pink background with glitter and shape of balloon with word Sori, Bas mitzvah, 75
+Layala Katz, Lakewood, 09/05/2021, Vanilla, Cupcake, Strawberry, no, Make it look nice!, Birthday, 28
+Sara Leah Levy, Brooklyn, 05/18/2021, Vanilla, Cupcake, Coconut, no, none, Engagement, 100
+Devorah Friedman, Brooklyn, 07/04/2021, Strawberry shortcake, Cake, no topping, no, none, Wedding, 15
+Kaufman, Lakewood, 11/09/2021, Chocolate peanut butter, Cake, Chocolate, no, none, Bar mitzvah, 3
+Chana Cohen, Lakewood, 07/04/2021, Banana, Cake, Vanilla, yes, Attached photo, Anniversary, 1
+Ahuva Licht, Lakewood, 06/22/2021, Sugar, Cookie, Royal icing, no, Write Chaim and Devorah, Engagement, 75
 Tziporah Markowitz, Lakewood, 03/16/2021, Strawberry shortcake, Cake, no topping, no, no, none, Baby, 3
 David Fried, Lakewood, 10/01/2021, Chocolate peanut butter, Cake, Chocolate, no, no, none, Bar mitzvah, 2
-Moshe Abrams, Brooklyn, 08/23/2021, Vanilla, Cupcake, Peanut butter, no, gf, Write 3 on it, Upsherin, 150
-Rachel Bernstein, Lakewood, 02/28/2021, Sugar, Cookie, Fondant, yes, gf, Attached photo of daughter, Bas mitzvah, 80
-Faiga Berg, Brooklyn, 10/12/2021, Chocolate, Cupcake, Chocolate, no, gf, Add a pecan on each one, Event, 350
-Dena Bergman, Brooklyn, 06/08/2021, Sugar, Cookie, Royal icing, no, no, Write Shimon and Leah, Engagement, 75
-Asher Yechiel Eisen, Brooklyn, 12/31/2021, Sugar, Cookie, Royal icing, no, no, In shape of tefillin, Bar mitzvah, 175
-Mendy Fischer, Lakewood, 07/04/2021, Banana, Cupcake, Vanilla, no, no, For bris, Baby, 100
-Chaya Kaplan, Lakewood, 01/01/2021, Chocolate, Cupcake, Vanilla, no, gf, Make it taste great please, Birthday, 100
-Sarala Schwartz, Brooklyn, 06/09/2021, Chocolate, Cupcake, Vanilla, no, gf, none, Baby, 50
-Sarah Braunstein, Brooklyn,  10/24/2021, Sugar, Cookie, Royal icing, yes, gf, See attached picture of my sukkah, Family party, 110
+Moshe Abrams, Brooklyn, 08/23/2021, Vanilla, Cupcake, Peanut butter, no, Write 3 on it, Upsherin, 150
+Rachel Bernstein, Lakewood, 02/28/2021, Sugar, Cookie, Fondant, yes, Attached photo of daughter, Bas mitzvah, 80
+Faiga Berg, Brooklyn, 10/12/2021, Chocolate, Cupcake, Chocolate, no, Add a pecan on each one, Event, 350
+Dena Bergman, Brooklyn, 06/08/2021, Sugar, Cookie, Royal icing, no, Write Shimon and Leah, Engagement, 75
+Asher Yechiel Eisen, Brooklyn, 12/31/2021, Sugar, Cookie, Royal icing, no, In shape of tefillin, Bar mitzvah, 175
+Mendy Fischer, Lakewood, 07/04/2021, Banana, Cupcake, Vanilla, no, For bris, Baby, 100
+Chaya Kaplan, Lakewood, 01/01/2021, Chocolate, Cupcake, Vanilla, no, Make it taste great please, Birthday, 100
+Sarala Schwartz, Brooklyn, 06/09/2021, Chocolate, Cupcake, Vanilla, no, none, Baby, 50
+Sarah Braunstein, Brooklyn,  10/24/2021, Sugar, Cookie, Royal icing, yes, See attached picture of my sukkah, Family party, 110
 */
 
